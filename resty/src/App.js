5f182d5851111d707ddf0f4015fd56708fe90862
rubabanat <rubabanat1996@gmail.com>
2021-05-02 21:29:35 +0300

    lab26

---
+++ resty/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './header';
+import Footer from './footer';
+import Form from './form';
+
+
+function App() {
+  return (
+ <> 
+ <Header/>
+ <Form/>
+ <Footer/>
+ </>
+  );
+}
+
+export default App;
---
+++ resty/src/footer.js
@@ -0,0 +1,11 @@
+import './footer.scss';
+
+const Footer =() =>{
+    return(
+    <footer>
+        <h1 className = 'footer'>&copy; 2021 Ruba Banat</h1>
+    </footer>
+    )
+}
+
+export default Footer;
